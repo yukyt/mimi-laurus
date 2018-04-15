d406a04949adc4d6525c452d010a64ae3e64e1ab
yuki <yuki@pridile.net>
2018-04-15 18:27:43 +0900

    use redux-immutable-state-invariant

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,11 +2,17 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers/index';
 
+
+// Be sure to ONLY add this middleware in development!
+const middleware = process.env.NODE_ENV !== 'production' ?
+  [require('redux-immutable-state-invariant').default(), thunk] : // eslint-disable-line global-require
+  [thunk];
+
 const configureStore = (initialState) => {
   const store = createStore(
     rootReducer,
     initialState,
-    applyMiddleware(thunk),
+    applyMiddleware(...middleware),
   );
   return store;
 };
