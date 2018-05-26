7648c897026ce8dafbc36c18cdd370c1d3dc761d
yuki <yuki@pridile.net>
2018-05-26 20:02:46 +0900

    #45 add description

--- src/containers/Comment.js
+++ src/containers/Comment.js
@@ -5,6 +5,8 @@ import * as CONSTANTS from '../define';
 
 const Comment = props => (
   <section style={{ display: props.viewMode === CONSTANTS.VIEW_MODE.COMMENT ? '' : 'none' }}>
+    <div>ゲスト投稿の場合、メールアドレスは架空のもので大丈夫です。</div>
+    <div>コメントフォームが表示されない場合、お手数ですが何度かリロードしてみてください。</div>
     <div id="disqus_thread" />
   </section>
 );
