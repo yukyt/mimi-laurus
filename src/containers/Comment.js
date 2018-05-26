d8de3eb7d4af3dbb266fdc9f68514ddf697b115a
yuki <yuki@pridile.net>
2018-05-26 19:00:24 +0900

    #45 Change plain disqus

--- src/containers/Comment.js
+++ src/containers/Comment.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
 import { connect } from 'react-redux';
-import ReactDisqusComments from 'react-disqus-comments';
 import * as CONSTANTS from '../define';
 
 const Comment = props => (
   <section style={{ display: props.viewMode === CONSTANTS.VIEW_MODE.COMMENT ? '' : 'none' }}>
-    <ReactDisqusComments
-      shortname="mimi-laurus"
-      identifier="mimi-laurus"
-      title="Mimi Laurus"
-      url="http://yukyt.github.io/mimi-laurus/"
-    />
+    <div id="disqus_thread" />
   </section>
 );
 
