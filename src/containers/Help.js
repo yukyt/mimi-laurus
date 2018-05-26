ed87cc9e2073e63f34818281ffa262bfd18428fd
yuki <yuki@pridile.net>
2018-05-26 18:06:52 +0900

    #17 Help update

--- src/containers/Help.js
+++ src/containers/Help.js
@@ -6,16 +6,13 @@ import * as CONSTANTS from '../define';
 const Help = props => (
   <section style={{ display: props.viewMode === CONSTANTS.VIEW_MODE.HELP ? '' : 'none' }}>
     <h3>Description</h3>
-    Mimi Laurusは<a href="https://miramiku.github.io/Laurus/" target="_blank" rel="noopener noreferrer">Laurus</a>（以下、本家）をスマホ向けに再構築したものです。
+    Mimi Laurusは<a href="https://miramiku.github.io/Laurus/" target="_blank" rel="noopener noreferrer">Laurus</a>（以下、オリジナル）をスマホ向けに再構築したものです。
     <ul>
-      <li>衣装、ステージのデータは本家のものを参照しています。</li>
+      <li>衣装、ステージのデータはオリジナルのものを参照しています。</li>
       <li>スマホ向けに一部機能は省略されています。</li>
-      <li>計算式などは本家のソースを元に模倣しております。（が、今はペナルティ、ブラックリストなどが非対応のため大きく異なります）</li>
-      <li>2018年5月現在、所持情報のデータ形式は本家と互換性があり、相互に書き出し/読み込みが可能です。</li>
+      <li>計算式などのオリジナルのソースを元に模倣しております。</li>
+      <li>2018年5月現在、所持情報のデータ形式はオリジナルと互換性があり、相互に書き出し/読み込みが可能です。</li>
     </ul>
-    <hr />
-    <h3>Inquiry</h3>
-    なにかあればGithubの<a href="https://github.com/yukyt/mimi-laurus/issues" target="_blank" rel="noopener noreferrer">Issue</a>にお願いします。
   </section>
 );
 
